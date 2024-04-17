74a74f66b510db2394a0259190aa331af48980cb
MoamenHamedM <moamen.h.abdellatef@gmail.com>
2024-04-17 15:08:04 +0200

    adding lin driver team 1

---
+++ Demo1/Team1/Src/HAL/LIN/LIN_MASTER/LIN_config.c
@@ -0,0 +1,68 @@
+
+#include "STD_Types.h"
+#include "HAL/LIN_Types.h"
+#include "LIN_cfg.h"
+#include "HAL/LIN_MASTER/LIN_Master_config.h"
+#include "HAL/LIN_SLAVE/LIN_Slave_config.h"
+
+static Message_8_Byte_Data_t MSG0Data;
+static Message_8_Byte_Data_t MSG1Data;
+
+static LIN_Signal_t Signals[_Signal_NUM] =
+    {
+        [First_signal] = {.Start_Index = 0, .Length = 8},
+        [Second_signal] = {.Start_Index = 8, .Length = 8}};
+
+/*
+static const LIN_Message_t Slave1_Messages[_SLAVE1_MSG_NUM] =
+    {
+        [First_MSG] =
+            {
+                .ID = 0x7,
+                .PID = 0x47,
+                .Data = MSG0Data,
+                .Data_Length = 2,
+                .Relation = Sender,
+                .Signals_Num = 2,
+                .Signals[0] = &Signals[0],
+                .Signals[1] = &Signals[2]
+
+            },
+        [Second_MSG] =
+            {
+                .ID = 0x5,
+                .PID = 0x85,
+                .Data = MSG1Data,
+                .Data_Length = 2,
+                .Relation = Receiver,
+                .Signals_Num = 1,
+                .Signals[0] = &Signals[0],
+            }};
+*/
+const LIN_Message_t Master_Messages[_MASTER_MSG_NUM] =
+    {
+        [First_MSG] =
+            {
+                .ID = 0x7,
+                .PID = 0x47,
+                .Data = MSG0Data,
+                .Data_Length = 2,
+                .Relation = Receiver,
+                .Signals_Num = 2,
+                .Signals[0] = &Signals[0],
+                .Signals[1] = &Signals[2]},
+        [Second_MSG] =
+            {
+                .ID = 0x5,
+                .PID = 0x85,
+                .Data = MSG1Data,
+                .Data_Length = 2,
+                .Relation = Sender,
+                .Signals_Num = 2,
+                .Signals[0] = &Signals[0],
+                .Signals[1] = &Signals[2]}};
+
+const LIN_SchedTableEntry_t SchedTable[SCHED_TABLE_MESSAGES_NUM] =
+    {
+        [0] = {.Message = &Master_Messages[0], .TimeSlotMs = 500},
+        [1] = {.Message = &Master_Messages[1], .TimeSlotMs = 600}};
