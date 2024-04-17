538633b9daef6c21d102875f52fe26a5b9f7a1af
MoamenHamedM <moamen.h.abdellatef@gmail.com>
2024-04-17 15:39:39 +0200

    adding lin cfg team 1

---
+++ Demo1/Team1/Inc/HAL/LIN_cfg.h
@@ -0,0 +1,14 @@
+#ifndef LIN_CFG_
+#define LIN_CFG_
+
+#define SCHED_TABLE_MESSAGES_NUM 2
+#define MASTER_CALL_PERIOD 5
+
+typedef enum
+{
+    First_signal,
+    Second_signal,
+    _Signal_NUM
+} Signals_t;
+
+#endif
